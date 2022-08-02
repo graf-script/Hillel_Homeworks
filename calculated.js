32733e473a94f96081a47523078fe557e5e7f453
Kirill Makhnovskyi <52702982+graf-script@users.noreply.github.com>
2022-08-02 21:43:39 +0300

    Homework_2

---
+++ calculated.js
@@ -0,0 +1,5 @@
+var enterNumber1 = parseInt(prompt('Enter first number:'));
+var enterNumber2 = parseInt(prompt('Enter second number:'));
+var result = enterNumber1 + enterNumber2;
+
+alert(`The result is: ${result}`);
