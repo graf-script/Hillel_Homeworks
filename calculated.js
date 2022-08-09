900939b422751c802d378dcff4b6f82e4e4f07e8
Kirill Makhnovskyi <52702982+graf-script@users.noreply.github.com>
2022-08-09 10:40:43 +0300

    Update calculated.js

--- calculated.js
+++ calculated.js
@@ -1,5 +1,5 @@
-var enterNumber1 = parseInt(prompt('Enter first number:'));
-var enterNumber2 = parseInt(prompt('Enter second number:'));
-var result = enterNumber1 + enterNumber2;
+let enterNumber1 = parseInt(prompt('Enter first number:'));
+let enterNumber2 = parseInt(prompt('Enter second number:'));
+let result = enterNumber1 + enterNumber2;
 
 alert(`The result is: ${result}`);
